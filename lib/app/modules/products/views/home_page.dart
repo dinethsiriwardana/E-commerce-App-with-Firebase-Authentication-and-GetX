de25a5fd61246ef245ee4bb74a321c9c905d12e0
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2024-11-29 20:22:32 +0530

    Create home_page.dart

---
+++ lib/app/modules/products/views/home_page.dart
@@ -0,0 +1,76 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:e_commerce_app/app/modules/products/controllers/product_controller.dart';
+import 'package:e_commerce_app/app/modules/products/views/product_details.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+
+class HomePage extends GetView<ProductController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Products')),
+      body: Obx(() {
+        if (controller.isLoading.value) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        if (controller.error.value.isNotEmpty) {
+          return Center(child: Text('Error: ${controller.error.value}'));
+        }
+
+        return GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 0.7,
+          ),
+          itemCount: controller.products.length,
+          itemBuilder: (context, index) {
+            final product = controller.products[index];
+            return GestureDetector(
+              onTap: () => Get.to(() => ProductDetailPage(product: product)),
+              child: Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: CachedNetworkImage(
+                        imageUrl: product.thumbnail,
+                        placeholder: (context, url) =>
+                            Center(child: CircularProgressIndicator()),
+                        errorWidget: (context, url, error) => Icon(Icons.error),
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            product.title,
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Text(
+                            '\$${product.price}',
+                            style: TextStyle(color: Colors.green),
+                          ),
+                          Text(
+                            product.brand,
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      }),
+    );
+  }
+}

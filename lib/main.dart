9ef9ebcabec76e657f2fdcb477b02971eb2d452b
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2024-11-29 22:33:08 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce_app/app/modules/products/controllers/auth_controller.dart';
+import 'package:e_commerce_app/app/routes/routes.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
@@ -6,8 +7,6 @@ import 'package:get/get.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
-  // Initialize AuthController
   Get.put(AuthController());
 
   runApp(MyApp());
@@ -18,8 +17,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'E-commerce App',
-      // initialRoute: AppPages.INITIAL,
-      // getPages: AppPages.routes,
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
     );
   }
 }
